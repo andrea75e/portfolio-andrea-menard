f1a0829777f7f3176d5d97e88fe462997c6c540b
andrea menard <andrea@Air-de-andrea.lan>
2026-08-24 21:36:27 +0200

    linkedin

--- src/composants/boutonlinkedin.tsx
+++ src/composants/boutonlinkedin.tsx
@@ -3,7 +3,7 @@
 // export default function BoutonLinkedIn() {
 //   return (
 //     <div className="flex items-center justify-center gap-6 mt-8">
-//       <a href="https://www.linkedin.com/in/andrea-menard-972320310" target="_blank" rel="noopener noreferrer">
+//       <a href="https://www.linkedin.com/in/andrea-menard" target="_blank" rel="noopener noreferrer">
 //         <img
 //           src={LinkedInLogo}
 //           className="w-16 h-16 transition-transform"
